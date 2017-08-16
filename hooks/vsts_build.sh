e2ed9433f40114c548d8933c2cc55a51d85dc92e
prashanthmadi <prashanthrmadi@gmail.com>
2017-08-16 16:27:09 -0500

    setting correct path for dockerfile

--- hooks/vsts_build.sh
+++ hooks/vsts_build.sh
@@ -2,5 +2,8 @@
 
 echo "About to change tag in Dockerfile"
 echo $1
+
+echo $PWD
+
 # docker tag in Dockerfile would be replaced by argument 
-sed -i 's@{{tag}}@'"$1"'@' Dockerfile
+sed -i 's@{{tag}}@'"$1"'@' ../Dockerfile
